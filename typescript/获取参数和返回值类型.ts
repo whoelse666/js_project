5e25a504515aad2524721b23c626364c694848ef
whoelse <347300979@qq.com>
2024-07-22 17:24:36 +0800

    update

--- typescript/获取参数和返回值类型.ts
+++ typescript/获取参数和返回值类型.ts
@@ -1,3 +1,4 @@
-import { login } from './api';
+// import { login } from './api';
+const login = new Promise((resolve, reject) => {});
 let options: Parameters<typeof login>[0];
 let resp:any|null = null;
